a543bba8d8868f2ae1685eeb73ba49338f997113
22002545CTC <22002545CTC@MA-CTC-INST-S19>
2023-12-18 13:42:09 -0500

    Hidden food

--- Snake/Images.cs
+++ Snake/Images.cs
@@ -9,10 +9,10 @@ namespace Snake
         public readonly static ImageSource Empty = LoadImage("Empty.png");
         public readonly static ImageSource Body = LoadImage("Body.png");
         public readonly static ImageSource Head = LoadImage("Head.png");
-        public readonly static ImageSource Food = LoadImage("Food.png");
+        public readonly static ImageSource Food = LoadImage("C:\\Users\\22002545CTC\\source\\repos\\Snake\\Snake\\Assets\\Food.png");
         public readonly static ImageSource DeadBody = LoadImage("DeadBody.png");
         public readonly static ImageSource DeadHead = LoadImage("DeadHead.png");
-        private static ImageSource LoadImage(string filename)
+        private static ImageSource LoadImage(string filename)   
         {
             return new BitmapImage(new Uri($"Assets/{filename}",
                 UriKind.Relative));
